81c5783db239ffcc54bf130be7b08b9defbcb8b8
Matt Hosack <hosack.matt@gmail.com>
2016-04-04 20:36:53 -0700

    Uncommenting tests.  Neibors is now part of test fixture

--- cpp_for_c_devs/part1/week2/tests/TestGraph.cpp
+++ cpp_for_c_devs/part1/week2/tests/TestGraph.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "graph.h"
+#include <list>
 
 class GraphFixture : public testing::Test {
 public:
@@ -10,6 +11,7 @@ public:
         delete g;
     }
     Graph *g;
+    std::list<int> neighbors;
 };
 
 TEST_F(GraphFixture, TestPasses){
@@ -35,22 +37,35 @@ TEST_F(GraphFixture, OnCreateNonDiagonalsAreNonAdjacent){
 
 TEST_F(GraphFixture, OnCreateNoVerticesValues){
     for(int i = 0; i < g->numVertices(); ++i){
-        ASSERT_EQ(0, g->getNodeValue(i));
+        ASSERT_EQ(0, g->getVertexValue(i));
     }
 }
 
 TEST_F(GraphFixture, SetVertexValueGetSameValue){
-    g->setNodeValue(1, 1234);
-    ASSERT_EQ(1234, g->getNodeValue(1));
+    g->setVertexValue(1, 1234);
+    ASSERT_EQ(1234, g->getVertexValue(1));
 }
 
 TEST_F(GraphFixture, OnCreateNoCosts){
-    ASSERT_EQ(g->getEdgeValue(0, 0), 0);
+    for(auto i = 0; i < g->numVertices(); ++i){
+        for(auto j = 0; j < g->numVertices(); ++j){
+            ASSERT_EQ(g->getEdgeValue(i, j), 0);
+        }
+    }
 }
 
 TEST_F(GraphFixture, SetCostGetSameCost){
     g->setEdgeValue(0, 0, 1234);
     ASSERT_EQ(g->getEdgeValue(0, 0), 1234);
+}
+
+TEST_F(GraphFixture, OnCreateNoNonDiagonalEdges){
+    for(auto i = 0; i < g->numVertices(); ++i){
+        for(auto j = 0; j < g->numVertices(); ++j){
+            if(i == j) continue;
+            ASSERT_FALSE(g->adjacent(i, j));
+        }        
+    }
 }
 
 TEST_F(GraphFixture, AddEdgeThenItExists){
@@ -67,49 +82,38 @@ TEST_F(GraphFixture, RemoveEdgeThenDoesNotExists){
 }
 
 TEST_F(GraphFixture, OnCreateNoNeighbors){
-    std::vector<int> result;
-    g->neighbors(1, result);
-    ASSERT_EQ(0, result.size());
+    neighbors = g->getNeighbors(1);
+    ASSERT_EQ(0, neighbors.size());
 }
 
 TEST_F(GraphFixture, AddEdgeOneNeighbor){
-    std::vector<int> result;
     g->add(1, 3);
-    g->neighbors(1, result);
-    ASSERT_EQ(1, result.size());
+    neighbors = g->getNeighbors(1);
+    ASSERT_EQ(1, neighbors.size());
 }
 
 TEST_F(GraphFixture, Add3Edges3Neighbors){
-    std::vector<int> result;
     g->add(1, 3);
     g->add(1, 8);
     g->add(1, 10);
-    g->neighbors(1, result);
-    ASSERT_EQ(3, result.size());
+    neighbors = g->getNeighbors(1);
+    ASSERT_EQ(3, neighbors.size());
 }
 
 TEST_F(GraphFixture, AddEdgeRemoveEdgeNeighborsEmpty){
-    // Why is the Graph Fixture retaining state????
-    g = new Graph(50);
-    // I shouldn't have to call new Graph(50) again...
-    std::vector<int> result;
     g->add(1, 3);
     g->remove(1, 3);
-    g->neighbors(1, result);
-    ASSERT_EQ(0, result.size());
+    neighbors = g->getNeighbors(1);
+    ASSERT_EQ(0, neighbors.size());
 }
 
 TEST_F(GraphFixture, AddEdgesRemoveEdgesNeighborsEmpty){
-    // Why is the Graph Fixture retaining state????
-    g = new Graph(50);
-    // I shouldn't have to call new Graph(50) again...
-    std::vector<int> result;
     g->add(1, 3);
     g->remove(1, 3);
     g->add(1, 4);
     g->remove(1, 4);
-    g->neighbors(1, result);
-    ASSERT_EQ(0, result.size());
+    neighbors = g->getNeighbors(1);
+    ASSERT_EQ(0, neighbors.size());
 }
 
 TEST_F(GraphFixture, NoNegativeCosts){
